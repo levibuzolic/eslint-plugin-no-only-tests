c61c0d01ad5dcec90f8306239d2bb876e5a88e10
Levi Buzolic <levibuzolic@gmail.com>
2016-02-18 15:43:01 +1100

    Updated tests, fixed readme

--- tests.js
+++ tests.js
@@ -1,10 +1,13 @@
-var rule = require('./rules/no-only-tests'),
+var rules = require('./index').rules;
 RuleTester = require('eslint').RuleTester;
 var ruleTester = new RuleTester();
-ruleTester.run('no-only-tests', rule, {
+
+ruleTester.run('no-only-tests', rules['no-only-tests'], {
   valid: [
     {
       code: 'describe("Some describe block", function() {});'
+    }, {
+      code: 'it("Some assertion", function() {});'
     }
   ],
 
@@ -14,6 +17,13 @@ ruleTester.run('no-only-tests', rule, {
       errors: [{
         message: '.only mocha test block found'
       }]
+    }, {
+      code: 'it.only("Some assertion", function() {});',
+      errors: [{
+        message: '.only mocha test block found'
+      }]
     }
   ]
 });
+
+console.log('Tests completed successfully');
