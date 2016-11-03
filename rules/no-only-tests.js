67226703ef6670c1bbda11d07187a9cf7b73865e
Brian Wendt <bwendt@locktonaffinity.com>
2016-11-03 10:07:15 -0500

    Support ESLint 3 rule format

--- rules/no-only-tests.js
+++ rules/no-only-tests.js
@@ -1,10 +1,16 @@
-module.exports = function(context) {
-  var regex = /^(describe|it)$/;
+module.exports = {
+  meta: {
+    docs: {}
+  },
 
-  return {
-    Identifier: function(node) {
-      if (node.name === 'only' && regex.test(node.parent.object.name)) {
-        context.report(node, node.parent.object.name + '.only not permitted');
+  create: function(context) {
+    var regex = /^(describe|it)$/;
+
+    return {
+      Identifier: function(node) {
+        if (node.name === 'only' && regex.test(node.parent.object.name)) {
+          context.report(node, node.parent.object.name + '.only not permitted');
+        }
       }
     }
   }
