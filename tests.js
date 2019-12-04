3354c54b7706945136d876bfdbdec7678d04b61b
Levi Buzolic <levibuzolic@gmail.com>
2019-12-04 12:19:02 +1100

    Add support for defining 2 levels deep in blocks (ie. `ava.default`)

--- tests.js
+++ tests.js
@@ -22,39 +22,54 @@ ruleTester.run('no-only-tests', rules['no-only-tests'], {
     {
       options: [{focus: ['focus']}],
       code: 'test.only("Options will exclude this from being caught", function() {});',
-    }
+    },
   ],
 
-  invalid: [{
-    code: 'describe.only("Some describe block", function() {});',
-    errors: [{message: 'describe.only not permitted'}]
-  }, {
-    code: 'it.only("Some assertion", function() {});',
-    errors: [{message: 'it.only not permitted'}]
-  }, {
-    code: 'context.only("Some context", function() {});',
-    errors: [{message: 'context.only not permitted'}]
-  }, {
-    code: 'test.only("Some test", function() {});',
-    errors: [{message: 'test.only not permitted'}]
-  }, {
-    code: 'tape.only("A tape", function() {});',
-    errors: [{message: 'tape.only not permitted'}]
-  }, {
-    code: 'fixture.only("A fixture", function() {});',
-    errors: [{message: 'fixture.only not permitted'}]
-  }, {
-    code: 'serial.only("A serial test", function() {});',
-    errors: [{message: 'serial.only not permitted'}]
-  }, {
-    options: [{block: ['obscureTestBlock']}],
-    code: 'obscureTestBlock.only("An obscure testing library test", function() {});',
-    errors: [{message: 'obscureTestBlock.only not permitted'}]
-  }, {
-    options: [{focus: ['focus']}],
-    code: 'test.focus("An alternative focus function", function() {});',
-    errors: [{message: 'test.focus not permitted'}]
-  }]
+  invalid: [
+    {
+      code: 'describe.only("Some describe block", function() {});',
+      errors: [{message: 'describe.only not permitted'}],
+    },
+    {
+      code: 'it.only("Some assertion", function() {});',
+      errors: [{message: 'it.only not permitted'}],
+    },
+    {
+      code: 'context.only("Some context", function() {});',
+      errors: [{message: 'context.only not permitted'}],
+    },
+    {
+      code: 'test.only("Some test", function() {});',
+      errors: [{message: 'test.only not permitted'}],
+    },
+    {
+      code: 'tape.only("A tape", function() {});',
+      errors: [{message: 'tape.only not permitted'}],
+    },
+    {
+      code: 'fixture.only("A fixture", function() {});',
+      errors: [{message: 'fixture.only not permitted'}],
+    },
+    {
+      code: 'serial.only("A serial test", function() {});',
+      errors: [{message: 'serial.only not permitted'}],
+    },
+    {
+      options: [{block: ['obscureTestBlock']}],
+      code: 'obscureTestBlock.only("An obscure testing library test", function() {});',
+      errors: [{message: 'obscureTestBlock.only not permitted'}],
+    },
+    {
+      options: [{block: ['ava.default']}],
+      code: 'ava.default.only("Block with dot", function() {});',
+      errors: [{message: 'ava.default.only not permitted'}],
+    },
+    {
+      options: [{focus: ['focus']}],
+      code: 'test.focus("An alternative focus function", function() {});',
+      errors: [{message: 'test.focus not permitted'}],
+    },
+  ],
 });
 
 console.log('Tests completed successfully');
